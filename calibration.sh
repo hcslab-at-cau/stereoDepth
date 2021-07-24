9f70798fb0a33e02d5d0bd969a4fe9a064939f40
gch04721 <gch0417@naver.com>
2021-07-24 22:10:01 +0900

    modify

--- calibration.sh
+++ calibration.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-path_root='iphone12'
+path_root='iphone11_calibration'
 pathImage="${path_root}/both"
 # realtime capture param
-left_addr='http://165.194.27.184:10003' # can using video dir
-right_addr='http://165.194.27.184:10004' # also
+left_addr='http://165.194.27.184:10001' # can using video dir
+right_addr='http://165.194.27.184:10002' # also
 image_format='png'
 
 # left camera parameters
@@ -22,21 +22,21 @@ height='7'
 # stereo calibration parameters
 pathStereoYML="${path_root}/stereo.yml"
 
-# # capture chessboard
-# echo "Launching capture chessboard"
-# python3 realtime_cap.py --left_source "$left_addr" --right_source "$right_addr" --left_prefix "$prefixImageLeft" --right_prefix "$prefixImageRight"\
-#  --image_dir "$path_root" --image_format "$image_format"
+capture chessboard
+echo "Launching capture chessboard"
+python3 realtime_cap.py --left_source "$left_addr" --right_source "$right_addr" --left_prefix "$prefixImageLeft" --right_prefix "$prefixImageRight"\
+ --image_dir "$path_root" --image_format "$image_format"
 
 
-# # left cam calibration
-# echo "Launching left single cam calibration"
-# python3 single_camera_calibration.py --image_dir "$pathImage" --image_format "$image_format" --prefix "$prefixImageLeft"\
-#  --square_size "$squareSize" --width "$width" --height "$height" --save_file "$pathLeftYML"
+# left cam calibration
+echo "Launching left single cam calibration"
+python3 single_camera_calibration.py --image_dir "$pathImage" --image_format "$image_format" --prefix "$prefixImageLeft"\
+ --square_size "$squareSize" --width "$width" --height "$height" --save_file "$pathLeftYML"
 
-# # right cam calibration
-# echo "Launching right single cam calibration"
-# python3 single_camera_calibration.py --image_dir "$pathImage" --image_format "$image_format" --prefix "$prefixImageRight"\
-#  --square_size "$squareSize" --width "$width" --height "$height" --save_file "$pathRightYML"
+# right cam calibration
+echo "Launching right single cam calibration"
+python3 single_camera_calibration.py --image_dir "$pathImage" --image_format "$image_format" --prefix "$prefixImageRight"\
+ --square_size "$squareSize" --width "$width" --height "$height" --save_file "$pathRightYML"
 
 # stere cam calibration
 echo "Launching stereo cam calibration"
--- run_depth.sh
+++ run_depth.sh
@@ -2,7 +2,7 @@
 
 dir_left="http://165.194.27.184:10001"
 dir_right="http://165.194.27.184:10002"
-calibration_file="iphone11_calibration/stereo_calib.yml"
+calibration_file="iphone12/stereo.yml"
 
 echo "run disparity map" 
 python3 stereo_depth.py --calibration_file "$calibration_file" --left_source "$dir_left" --right_source "$dir_right"\
